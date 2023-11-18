d9d96c17aefacfbded01f13fda6285c2a0575026
Josh <josh@nerdy.co.nz>
2023-11-18 13:13:04 +1300

    clean out licensed icons

--- desktop/src/render/store/index.ts
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'vuex'
-
-// export const key: InjectionKey<Store<RelapseModule>> = Symbol()
-
-// const store = serverEvents.registerEvents(store)
-export const store = createStore({})
